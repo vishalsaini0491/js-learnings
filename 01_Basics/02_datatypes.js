c3696adda906e33e155693ba60bc36fbdcef5883
Vishal Saini <86426716+vishalsaini0491@users.noreply.github.com>
2025-03-06 20:27:09 +0000

    lec 5

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // TREAT ALL JS CODE AS NEWER VERSION
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Vishal")
+
+/**
+ *      DOCUMENTATIONS:
+ *      1. TC 39 : https://tc39.es/ecma262/
+ *      2. MDN
+ */
+
+// datatypes
+
+let name = "hitesh" // string
+let age = 18     // int
+let isLoggedIn  = false  // boolean
+
+// -----------------Primitive datatypes---------------------
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null = stand alone value (type = object)
+// undefined = kindda place holder for a value which is not defined yet (type = undefined)
+// symbol => uniquess
+
+// object
+
+// ----------------------------------------------------------
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
