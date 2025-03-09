f0bc83ab3dbc3c2a431266c6adcd9156d63b2af3
Vishal Saini <86426716+vishalsaini0491@users.noreply.github.com>
2025-03-09 22:40:27 +0000

    not completed yet

---
+++ 01_Basics/05_datatypesSummary.js
@@ -0,0 +1,18 @@
+// Differentiation of types of datypes ----> 1. Primitive datatypes      2. Non Primitive Datatypes
+
+// Done on the basis of two things:
+
+// 1. On the basis of how they are stored in memory
+// 2. And how we can access our data
+
+
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non Primitive Datatypes :- 
+// Reference (Non Primitive)
+
+// Array, objects, functions
+
+
