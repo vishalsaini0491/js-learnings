356fe0ceea5ec0a34d4282150cceb9ec0d93b7f3
Vishal Saini <86426716+vishalsaini0491@users.noreply.github.com>
2025-03-10 20:09:51 +0000

    dsc

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -9,3 +9,21 @@ console.log(`hello, his name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hiteshhc'); 
 // we are basically creating object of string type in heap
+
+// console.log(gameName[0]);
+// console.log(gameName.___proto__);
+
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
+//  console.log(gameName.charAt(2));
+//  console.log(indexOf('t'));
+
+const newString = gameName.substring(0,4);  // can't use negative value
+console.log(newString);
+
+const anotherString = gameName.slice(0,4);
+console.log(anotherString);
+
+const anotheranotherString = gameName.slice(-7,4);
+console.log(anotheranotherString);
+
