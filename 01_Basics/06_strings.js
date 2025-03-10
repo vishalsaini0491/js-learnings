0d5bb41289d6b4b74ff104918ab98fd7fb60aae0
Vishal Saini <86426716+vishalsaini0491@users.noreply.github.com>
2025-03-10 19:06:03 +0000

    lec 11 mid

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + 12);
+
+console.log(`hello, his name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc'); 
+// we are basically creating object of string type in heap
