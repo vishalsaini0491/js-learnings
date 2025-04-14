32f3e7693b2d24e56860d1a573db1e9180422001
Vishal Saini <86426716+vishalsaini0491@users.noreply.github.com>
2025-04-14 10:32:11 +0000

    ..

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,6 +1,12 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"];
 const dc_heros = ["superman", "flash", "batman"];
 
-marvel_heros.push(dc_heros); // this push the new array like this: [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
-console.log(marvel_heros);
+/*
+marvel_heros.push(dc_heros); // // don't merges both arrays, this push the new array like this: [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+console.log(marvel_heros); 
 console.log(marvel_heros[3][1]);
+
+*/
+// OTHER SYNTAX FOR DOING ABOVE THING
+marvel_heros.concat(dc_heros); // this returns new arry and not directly merge to 
+console.log(marvel_heros);
